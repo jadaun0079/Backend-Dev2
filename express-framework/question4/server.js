8cd032ee90761f77ec5141197095e7f56bb76569
jadaun0079 <jatinpratapsingh1000@gmail.com>
2026-02-10 20:20:06 +0530

    question4

--- express-framework/question4/server.js
+++ express-framework/question4/server.js
@@ -3,13 +3,13 @@ import mainRoutes from "./routes/mainRoutes.js";
 
 const app = express();
 
-// EJS setup
+
 app.set("view engine", "ejs");
 
-// Routes
+
 app.use("/", mainRoutes);
 
-// ✅ Custom 404 Middleware (always last)
+
 app.use((req, res) => {
   res.status(404).render("404", { url: req.originalUrl });
 });
